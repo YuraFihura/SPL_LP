5680528f1d166c22655729cf665c7e1530e1d01d
Pivinter <vlad.kulia@gmail.com>
2024-11-09 12:45:22 +0200

    Додавання файлів

---
+++ Data/Lab8/Constants/config.py
@@ -0,0 +1,3 @@
+# Constants/config.py
+CSV_FILE_PATH = '/home/vladusald/PycharmProjects/СМП Л1/Data/Lab8/Assets/your_file.csv'
+IMAGE_FILE_PATH = '/home/vladusald/PycharmProjects/СМП Л1/Data/Lab8/Assets/sales_amount_interactive.png'
